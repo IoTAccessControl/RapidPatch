0c1ec6f5cc82616855639f3c812ebf7726b21f0b
CBackyx <cbackyx@gmail.com>
2021-10-29 13:16:43 +0800

    Update QEMU test script

--- Docker/test_rapidpatch.sh
+++ Docker/test_rapidpatch.sh
@@ -1,6 +1,6 @@
-cd /work/VulDevices/qemu-stm32-p103/ && \
-    [ -d build ] && rm -r build && \ 
-    mkdir build && \
+cd /work/VulDevices/qemu-stm32-p103/
+[ -d build ] && rm -r build
+mkdir build && \
     cd build && \
     cmake .. && \
     make qemu
